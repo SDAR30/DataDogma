b3140390f5643366a066adb2f99968f39db37b2d
Shoaib Dar <SDAR30@gmail.com>
2023-09-26 17:43:34 -0400

    setup footer template with subscription submit button

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import Header from '@/src/components/Header';
 import './globals.css'
 import { Inter, Manrope } from 'next/font/google'
 import {cx} from '@/src/utils'
+import Footer from '../components/Footer';
 
 const inter = Inter({ subsets: ['latin'], display: "swap", variable: "--font-in" })
 
@@ -21,6 +22,7 @@ export default function RootLayout({ children }) {
       >
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   )
