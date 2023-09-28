d59e6c241ccce4d1e80d03f078169f4b7b10d0bf
Shoaib Dar <SDAR30@gmail.com>
2023-09-27 21:16:11 -0400

    implement code captions, titles, and fragments in code blocks

--- contentlayer.config.js
+++ contentlayer.config.js
@@ -1,55 +1,65 @@
-import { makeSource, defineDocumentType} from '@contentlayer/source-files'
+import { makeSource, defineDocumentType } from '@contentlayer/source-files'
 import readingTime from 'reading-time';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import rehypePrettyCode from 'rehype-pretty-code';
+import rehypeSlug from 'rehype-slug';
+import remarkGfm from 'remark-gfm';
 
 const Blog = defineDocumentType(() => ({
-    name: 'Blog',
-    filePathPattern: '**/**/*.mdx',
-    contentType: "mdx",
-    fields: {
-      title: {
-        type: 'string',
-        required: true,
-      },
-      publishedAt: {
-        type: 'date',
-        required: true,
-      },
-      updatedAt: {
-        type: 'date',
-      },
-      description: {
-        type: 'string',
-        required: true,
-      },
-      image: {
-        type: 'image'
-      },
-      isPublished: {
-        type: 'boolean',
-        required: true,
-      },
-      author: {
-        type: 'string',
-        required: true
-      },
-      tags: {
-        type: 'list',
-        of: {type: 'string'}
-      },
+  name: 'Blog',
+  filePathPattern: '**/**/*.mdx',
+  contentType: "mdx",
+  fields: {
+    title: {
+      type: 'string',
+      required: true,
     },
-    computedFields: {
-        url: {
-          type: 'string',
-          resolve: (doc) => `/blogs/${doc._raw.flattenedPath}`,
-        },
-        readingTime: {
-          type: "json",
-          resolve: (doc) => readingTime(doc.body.raw)
-        },
-      },
-  }));
+    publishedAt: {
+      type: 'date',
+      required: true,
+    },
+    updatedAt: {
+      type: 'date',
+    },
+    description: {
+      type: 'string',
+      required: true,
+    },
+    image: {
+      type: 'image'
+    },
+    isPublished: {
+      type: 'boolean',
+      required: true,
+    },
+    author: {
+      type: 'string',
+      required: true
+    },
+    tags: {
+      type: 'list',
+      of: { type: 'string' }
+    },
+  },
+  computedFields: {
+    url: {
+      type: 'string',
+      resolve: (doc) => `/blogs/${doc._raw.flattenedPath}`,
+    },
+    readingTime: {
+      type: "json",
+      resolve: (doc) => readingTime(doc.body.raw)
+    },
+  },
+}));
+
+const codeOptions = { theme: 'github-dark'}
 
 export default makeSource({
-    contentDirPath: 'content',
-    documentTypes: [Blog],
+  contentDirPath: 'content',
+  documentTypes: [Blog],
+  mdx: {
+    remarkPlugins: [remarkGfm], rehypePlugins: [rehypeSlug, [rehypeAutolinkHeadings, { behavior: "append" }],
+      [rehypePrettyCode, codeOptions]]
+  },
 })
